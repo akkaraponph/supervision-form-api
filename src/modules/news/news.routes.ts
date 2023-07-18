597e0e52a2c51a82ef9cc8401c2704a46e9134f7
tamwannida <125627340+tamwannida@users.noreply.github.com>
2023-07-18 15:50:38 +0700

    edit API  updateByMe

--- src/modules/news/news.routes.ts
+++ src/modules/news/news.routes.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "../../middlewares/auth.midleware";
 import newsController from './news.controllers'
 const router = Router();
 
-router.get('/:id', newsController.getOne)
+router.get('/id/:id', newsController.getOne)
 router.get('/', newsController.getAll)
 router.post('/', newsController.create)
 router.patch('/:id', newsController.update)
